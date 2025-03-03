dffb5c65241b9083a45feea6102554a32c5e90fa
yuki5718 <hayate8575@gmail.com>
2025-03-03 17:55:22 +0800

    建立專案

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h1>第七周</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
