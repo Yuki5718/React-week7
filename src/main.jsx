1932096900f915bdcea6e4294fcab545c6ccc7ad
yuki5718 <hayate8575@gmail.com>
2025-03-04 02:59:21 +0800

    移除 StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import store from './redux/store'
 import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>    
-  </StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
