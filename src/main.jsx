5cbaae5059a83d0eb73f4104bebd03db28992df2
yuki5718 <hayate8575@gmail.com>
2025-03-03 19:10:09 +0800

    新增Redux：Provider & store

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>    
   </StrictMode>,
 )
